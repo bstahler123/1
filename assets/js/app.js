010057748908e094900d4ee774414de11d6e8c6e
brad stahler <bradstahler@brads-MacBook-Air.local>
2017-06-08 03:13:32 -0400

    everything working

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+$(document).on('click', 'a', function(event) {
+
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+
+$(document).ready(function() {
+
+    const config = {
+        apiKey: "AIzaSyD5eqGeGoJKiDUXQA36Qi5_Qtoqs3aVqL0",
+        authDomain: "portfolio-messenger.firebaseapp.com",
+        databaseURL: "https://portfolio-messenger.firebaseio.com",
+        projectId: "portfolio-messenger",
+        storageBucket: "portfolio-messenger.appspot.com",
+        messagingSenderId: "51564714549"
+    };
+    firebase.initializeApp(config);
+    $(".form1").on('submit', event => {
+        event.preventDefault();
+        const name = $('#from_name').val();
+        const email = $('#from_email').val();
+        const message = $('#comments').val();
+        console.log(name, email, message);
+        firebase.database().ref('clients').push({
+            name,
+            email,
+            message
+
+        });
+        $(".alert-success").show().fadeOut(4000);
+        document.getElementById('from_name').value = "";
+        document.getElementById('from_email').value = "";
+        document.getElementById('comments').value = "";
+        
+
+
+
+
+        
+    });
+
+
+});
